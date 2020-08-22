e543572994e2f91de0c0fe8ad35d36377cc666f5
tiiedye <tiiedye@gmail.com>
2020-08-22 09:40:03 -0700

    finished video

--- NBAapp/src/components/games/article.js
+++ NBAapp/src/components/games/article.js
@@ -9,12 +9,43 @@ import {
 } from 'react-native';
 import Video from 'react-native-video';
 import Icon from 'react-native-vector-icons/dist/Ionicons';
+import {connect} from 'react-redux';
+import {autoSignIn} from '../../store/actions/user_actions';
+import { getTokens, setTokens} from '../../utils/misc';
+import Puppy from '../../assets/Puppy.mp4';
 
-export default class  GameArticleComponent extends Component {
+class  GameArticleComponent extends Component {
 
   state = {
     loading: false,
     isAuth: false
+  }
+
+  manageState(loading, isAuth) {
+    this.setState({
+      loading,
+      isAuth
+    })
+  }
+
+  componentDidMount() {
+    const User = this.props.User;
+
+    getTokens((value) =>{
+      if(value[0][1] === null) {
+        this.manageState(false, false)
+      } else {
+        this.props.dispatch(autoSignIn(value[0][1]))
+          .then(() => {
+            !User.auth.token ?
+              this.manageState(false, false)
+            :
+              setTokens(User.auth, () => {
+                this.manageState(false, true)
+              })
+          })
+      }
+    })
   }
 
   render(){
@@ -31,7 +62,12 @@ export default class  GameArticleComponent extends Component {
         <ScrollView style={{backgroundColo: '#F0F0F0'}}>
           {
             this.state.isAuth ?
-              <Text>Video</Text>
+              <Video
+                source={Puppy}
+                muted={true}
+                pause={true}
+                style={styles.video}
+              />
             :
               <View style={styles.notAuth}>
                 <Icon name="md-sad" size={80} color="#d5d5d5" />
@@ -66,5 +102,17 @@ const styles = StyleSheet.create({
   },
   notAuthText: {
     fontFamily: 'Roboto-Bold'
+  },
+  video: {
+    width: '100%',
+    height: 250
   }
 });
+
+function mapStateToProps(state){
+  return {
+      User:state.User
+  }
+}
+
+export default connect(mapStateToProps)(GameArticleComponent);
